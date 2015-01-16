2fc03f767266207f410d513c1caa98b355c9ea82
Oliver Charles <ollie@ocharles.org.uk>
2015-01-16 19:51:16 +0000

    Regenerate nix expressions

--- default.nix
+++ default.nix
@@ -1,16 +1,17 @@
-{ haskellPackages ? (import <nixpkgs> {}).haskellPackages }:
-let
-  inherit (haskellPackages) cabal cabalInstall_1_18_0_2
-    hsyslog pipes pipesSafe text uuid uniplate unixBytestring unorderedContainers vector;
-
-  pkgs = import <nixpkgs> {};
-
-in cabal.mkDerivation (self: {
+{ mkDerivation, base, bytestring, hashable, hsyslog, pipes
+, pipes-safe, stdenv, systemd, text, transformers, uniplate
+, unix-bytestring, unordered-containers, uuid, vector
+}:
+mkDerivation {
   pname = "libsystemd-journal";
-  version = "1.0.0";
+  version = "1.3.1";
   src = ./.;
-  buildDepends = [ hsyslog pipes pipesSafe text uuid unixBytestring uniplate unorderedContainers vector ];
-  extraLibraries = [ pkgs.systemd ];
-  buildTools = [ cabalInstall_1_18_0_2 ];
-  enableSplitObjs = false;
-})
+  buildDepends = [
+    base bytestring hashable hsyslog pipes pipes-safe text transformers
+    uniplate unix-bytestring unordered-containers uuid vector
+  ];
+  pkgconfigDepends = [ systemd ];
+  homepage = "http://github.com/ocharles/libsystemd-journal";
+  description = "Haskell bindings to libsystemd-journal";
+  license = stdenv.lib.licenses.bsd3;
+}
